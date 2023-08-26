cc0a03e01dd3ebd3ef6a047b4c7b4aab0cac5cc2
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-08-26 20:07:52 -0300

    nullable types

---
+++ CSFundamentos1/NullableTypes1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("## Nullable Types ##\n");
+
+//int? i = null;
+//double? d = null;
+//bool? b = null;
+
+//Console.WriteLine(i);
+//Console.WriteLine(d);
+//Console.WriteLine(b);
+
+#region Erro de execução
+
+//Esses trechos darão erro pois não é possível converter um int? para int
+
+//int? a = null;
+//int b = a;
+
+//int? x = 4;
+//int y = 3;
+//int z = x * y;
+//Console.WriteLine(z);
+
+#endregion
+
+int? b = 100;
+if (b.HasValue)
+{
+    Console.WriteLine($"b = {b.Value}");
+}
+else
+{
+    Console.WriteLine("b não possui valor");
+}
+
+Console.ReadKey();
