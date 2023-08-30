eb3841b342448e27b8a4f7e02fa7c9cab3b871de
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-08-30 15:36:46 -0300

    conversão de tipos

---
+++ CSFundamentos1/1ConversaoTipos/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("## Conversão de tipos ##\n");
+
+//int varInt = 100;
+//double varDouble = varInt;
+
+//Console.WriteLine(varDouble);
+
+//int numeroInt = 2145678;
+//long numeroLong = numeroInt;
+//float numeroFloat = numeroInt;
+//double numeroDouble = numeroInt;
+//decimal numeroDecimal = numeroInt;
+
+//Console.WriteLine(numeroInt);
+//Console.WriteLine(numeroLong);
+//Console.WriteLine(numeroFloat);
+//Console.WriteLine(numeroDouble);
+//Console.WriteLine(numeroDecimal);
+
+double varDouble = 12.456;
+int varInt = (int)varDouble;
+
+Console.WriteLine(varInt);
+
+int num1 = 10;
+int num2 = 4;
+
+float resultado = (float) num1 / num2;
+
+Console.WriteLine(resultado);
+
+Console.ReadLine();
