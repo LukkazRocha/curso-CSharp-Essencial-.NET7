5554b724149443ac24e631320fa4d7c8b4ec6174
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-08-31 16:14:32 -0300

    estudos no .net7
    - Constantes
    - Operadores

---
+++ CSFundamentos1/Constantes1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("## Constantes ##\n");
+
+//Cálculo da área e perímetro do circulo
+
+double raio, perimetro, area;
+
+const double PI = 3.14;
+
+Console.WriteLine("Informe o raio do círculo");
+raio = Convert.ToDouble(Console.ReadLine());
+
+perimetro = 2 * PI * raio;
+area = PI * raio * raio;
+
+Console.WriteLine($"Perímetro = {perimetro}");
+Console.WriteLine($"Area = {area}");
+
+Console.ReadKey();
