2941ef92bec1e93a72edb2f2e65f7cd9108aad49
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-08-31 16:27:44 -0300

    procedendia e associatividade de operadores

---
+++ CSFundamentos1/ProcedenciaOperadores1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("## Procedência de operadores ##\n");
+
+//int x = (10 - 2) * 3;
+//Console.WriteLine(x);
+//Console.WriteLine();
+
+//bool b = !(9 != 8) && (5 >= 7 || 8 >= 6);
+
+//Console.WriteLine(b);
+
+int a = 5, b = 6, c = 4;
+
+int r = --a * b - ++c;
+
+Console.WriteLine(r);
+
+Console.ReadLine();
