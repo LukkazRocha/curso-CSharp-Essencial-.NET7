b1fddc3f98f676feb9be366f543fa8ba265dd4e5
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-02 17:13:07 -0300

    inicia estudos sobre estruturas de controle

---
+++ EstruturasControle/3Estrutura_If_else_If/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("## Instrução if-else ##\n");
+
+Console.WriteLine("\nInforme a nota do aluno \t");
+var nota = Convert.ToDouble(Console.ReadLine());
+
+if (nota < 5)
+{
+    Console.WriteLine("\nALUNO REPROVADO");
+}
+else if (nota >= 5 && nota < 6)
+{
+    Console.WriteLine("\nALUNO EM RECUPERAÇÃO");
+}
+else if (nota >= 6 && nota <= 9)
+{
+    Console.WriteLine("\nALUNO APROVADO");
+}
+else
+{
+    Console.WriteLine("\nALUNO APROVADO COM DESTINÇÃO");
+}
+Console.WriteLine("\nFim do processamento...");
+
+Console.ReadKey();
