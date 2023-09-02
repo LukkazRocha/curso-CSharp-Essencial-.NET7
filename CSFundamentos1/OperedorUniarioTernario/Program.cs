29ba438c794eeca86abba6064c3e32e0e9793aba
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-02 15:25:08 -0300

    finaliza tópico de fundamentos no c#

---
+++ CSFundamentos1/OperedorUniarioTernario/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("## Operador Uniário e Ternário ##\n");
+
+//int positivo = 1;
+//int resultado;
+
+//resultado = +positivo;
+//Console.WriteLine(resultado);
+
+//Console.WriteLine("Informe o número: \n");
+//var n = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine($"O negativo de {n} é {-n}");
+
+//////////////////////////////////////////////////
+
+//Console.WriteLine("Informe a temperatura: \n");
+//var temp = Convert.ToDouble(Console.ReadLine());
+
+//var resultado = temp > 27 ? "Quente" : "Normal";
+
+//Console.WriteLine($"O tempo está {resultado}");
+
+/////////////////////////////////////////////////
+
+Console.WriteLine("Informe o valor de x");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Informe o valor de y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string resultado = x > y ? "x é maior que y" :
+                   x < y ? "x é menor que y" :
+                   x == y ? "x é igual a y" : "Sem resultado";
+
+Console.WriteLine(resultado);
+
+Console.ReadLine();
