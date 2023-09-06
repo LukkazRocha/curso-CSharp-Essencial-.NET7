db20613e5c9d26793a17da19346bf3205432f8d7
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-06 17:46:11 -0300

    estudos sobre métodos e parâmetros

---
+++ ClassesMetodos/3MetodosParametros/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## Métodos ##\n");
+
+MinhaClasse minhaClasse = new MinhaClasse();
+
+var nomeCliente = "Maria";
+var dataAtual = DateTime.Now.ToShortDateString();
+
+//valores dos argumentos
+minhaClasse.Saudacao(nomeCliente, dataAtual);
+
+Console.ReadKey();
+
+class MinhaClasse
+{
+    //os parâmetros do método
+    public void Saudacao(string nome, string data)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(data);
+    }
+}
--- ClassesMetodos/ClassesMetodos/Program.cs
+++ ClassesMetodos/ClassesMetodos/Program.cs
@@ -1,18 +1,18 @@
 ﻿Console.WriteLine("## Classes e Métodos ##\n");
 
-int x = 100;
-
-Pessoa p1 = new Pessoa();
+Pessoa p1 = new();
 p1.nome = "Maria";
 p1.idade = 25;
 p1.sexo = "feminino";
 
 Console.WriteLine($"{p1.nome} {p1.idade} {p1.sexo}");
 
-Pessoa p2 = new Pessoa();
-p2.nome = "Manuel";
-p2.idade = 23;
-p2.sexo = "masculino";
+Pessoa p2 = p1;
+
+//Pessoa p2 = new Pessoa();
+//p2.nome = "Manuel";
+//p2.idade = 23;
+//p2.sexo = "masculino";
 
 Console.WriteLine($"{p2.nome} {p2.idade} {p2.sexo}");
 
