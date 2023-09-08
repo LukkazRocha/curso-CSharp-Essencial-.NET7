4fa20091a5003cca2cdc98ec0777190d287d585c
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-08 17:00:40 -0300

    inicia estudos sobre classe e realiza primeiro exercicio

---
+++ ClassesMetodos/6This/Program.cs
@@ -0,0 +1,63 @@
+﻿Console.WriteLine("## This ##\n");
+
+//public class Cliente
+//{
+//    public string? Nome;
+//    public int Idade;
+
+//    public Cliente(string? Nome, int Idade)
+//    {
+//        this.Nome = Nome;
+//        this.Idade = Idade;
+//    }
+//}
+
+///////////////////////////////////////////////
+
+//Teste t1 = new Teste();
+//t1.Num1 = 10;
+//t1.Num2 = 20;
+//t1.Exibir();
+
+//Teste t2 = new Teste();
+//t2.Num1 = 88;
+//t2.Num2 = 99;
+//t2.Exibir();
+
+//public class Teste
+//{
+//    public int Num1;
+//    public int Num2;
+
+//    public void PassarParametro(Teste t)
+//    {
+//        Console.WriteLine($"\nthis = {this}");
+//        Console.WriteLine("Num1: " + t.Num1);
+//        Console.WriteLine("Num2: " + t.Num2);
+//    }
+
+//    public void Exibir()
+//    {
+//        // passando this como um parâmetro (a classe Teste)
+//        PassarParametro(this);
+//    }
+//}
+
+///////////////////////////////////////////////
+
+Teste t1 = new Teste(999);
+
+Console.ReadKey();
+
+public class Teste
+{
+    public Teste(int num1, int num2)
+    {
+        Console.WriteLine($"\nConstrutor com dois parâmetros {num1} e {num2}");
+    }
+
+    public Teste(int num) : this(777, 888)
+    {
+        Console.WriteLine("\nConstrutor com um parâmetro " + num);
+    }
+}
---
+++ Exercicios/01ExercicioPratico/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.WriteLine("## Exercício Prático 01 ##\n");
+
+//Carro chevrolet = new Carro();
+//chevrolet.modelo = "Sedan";
+//chevrolet.montadora = "Chevrolet";
+//chevrolet.marca = "Onix";
+//chevrolet.ano = 2016;
+//chevrolet.potencia = 110;
+
+//Carro ford = new Carro();
+//ford.modelo = "SUV";
+//ford.montadora = "Ford";
+//ford.marca = "EcoSport";
+//ford.ano = 2018;
+//ford.potencia = 120;
+
+//Console.WriteLine(chevrolet.modelo);
+//Console.WriteLine(chevrolet.montadora);
+//Console.WriteLine(chevrolet.marca);
+//Console.WriteLine(chevrolet.ano);
+//Console.WriteLine(chevrolet.potencia);
+//chevrolet.Acelerar(chevrolet.marca);
+
+//Console.WriteLine();
+
+//Console.WriteLine(ford.modelo);
+//Console.WriteLine(ford.montadora);
+//Console.WriteLine(ford.marca);
+//Console.WriteLine(ford.ano);
+//Console.WriteLine(ford.potencia);
+//ford.Acelerar(ford.marca);
+
+///////////////////////////////////////
+
+Carro chevrolet = new Carro("Sedan", "Chevrolet", "Onix", 2016, 110);
+Carro ford = new Carro("SUV", "Ford", "EcoSport", 2018, 120);
+
+Console.WriteLine($"{chevrolet.modelo} {chevrolet.montadora} {chevrolet.marca} {chevrolet.ano} {chevrolet.potencia} CV");
+chevrolet.Acelerar(chevrolet.marca);
+
+Console.WriteLine($"{ford.modelo} {ford.montadora} {ford.marca} {ford.ano} {ford.potencia} CV");
+ford.Acelerar(ford.marca);
+
+public class Carro
+{
+    public string? modelo;
+    public string? montadora;
+    public string? marca;
+    public int ano;
+    public int potencia;
+
+    public Carro(string? modelo, string? montadora, string? marca, int ano, int potencia)
+    {
+        this.modelo = modelo;
+        this.montadora = montadora;
+        this.marca = marca;
+        this.ano = ano;
+        this.potencia = potencia;
+    }
+
+    public void Acelerar(string marca)
+    {
+        Console.WriteLine($"Acelerando o meu {marca}\n");
+    }
+}
