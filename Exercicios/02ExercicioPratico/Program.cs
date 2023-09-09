c493cfcde900d25136252555469afa09b3404510
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-09 16:34:36 -0300

    exercicio 02

---
+++ Exercicios/02ExercicioPratico/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("## Exercício Prático 02 ##\n");
+
+Carro chevrolet = new Carro("Sedan", "Chevrolet", "Onix", 2016, 110);
+Carro ford = new Carro("SUV", "Ford", "EcoSport", 2018, 120);
+
+chevrolet.VelocidadeMaxima(chevrolet.Potencia);
+ford.VelocidadeMaxima(ford.Potencia);
+
+public class Carro
+{
+    public string? Modelo;
+    public string? Montadora;
+    public string? Marca;
+    public int Ano;
+    public int Potencia;
+
+    public Carro(string? modelo, string? montadora)
+    {
+        Modelo = modelo;
+        Montadora = montadora;
+    }
+
+    public Carro(string? modelo, string? montadora, string? marca, int ano, int potencia)
+    {
+        Modelo = modelo;
+        Montadora = montadora;
+        Marca = marca;
+        Ano = ano;
+        Potencia = potencia;
+    }
+
+    public void Acelerar(string marca)
+    {
+        Console.WriteLine($"Acelerando o meu {marca}\n");
+    }
+
+    public double VelocidadeMaxima(int potencia)
+    {
+        return potencia * 1.75;
+    }
+}
