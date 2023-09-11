44f0b691f997ce988f3c6244ccc381425f4903b4
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-11 18:37:10 -0300

    passando variavel por referencia utilizando OUT

---
+++ ClassesMetodos/12PassandoArgumentosPorReferencia/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("## Passando argumentos por valor ##\n");
+
+int x = 20;
+
+Console.WriteLine("\nValor do argumento x antes de passar por referência " + x);
+
+Calculo calc = new();
+calc.Dobrar(ref x);
+
+Console.WriteLine("\nValor do argumento x depois de passar por referência " + x);
+
+Console.ReadKey();
+
+public class Calculo
+{
+    public void Dobrar(ref int y)
+    {
+        y *= 2;
+        Console.WriteLine("\nValor do parâmetro y no método Dobrar " + y);
+    }
+}
