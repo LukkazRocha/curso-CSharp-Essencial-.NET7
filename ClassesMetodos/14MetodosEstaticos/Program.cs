8b15097ff845a114794c0ce26d05d404cd7fbd4d
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-12 15:20:17 -0300

    orgaliza lista de aulas

---
+++ ClassesMetodos/14MetodosEstaticos/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("## Métodos estáticos ##\n");
+
+var soma = Calculadora.Somar(10, 10);
+var subtracao = Calculadora.Subtrair(10, 10);
+var multiplicacao = Calculadora.Multiplicar(10, 10);
+var divisao = Calculadora.Dividir(10, 10);
+
+Console.WriteLine(soma);
+Console.WriteLine(subtracao);
+Console.WriteLine(multiplicacao);
+Console.WriteLine(divisao);
+
+Console.ReadKey();
+
+public class Calculadora
+{
+    public static int Somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+    public static int Subtrair(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+
+    public static int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+
+    public static int Dividir(int n1, int n2)
+    {
+        return n1 / n2;
+    }
+}
