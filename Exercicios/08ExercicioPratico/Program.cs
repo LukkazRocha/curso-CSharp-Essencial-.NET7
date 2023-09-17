90286497fe39c2dcd896347043535a229f186019
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-17 01:13:50 -0300

    realizando exercicio 08

---
+++ Exercicios/08ExercicioPratico/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("## Exercício Prático 08 ##\n");
+
+Cliente cliente = new("Lucas", "lucas@email.com", 29);
+Cliente.ExibirInfo(email: cliente.Email, nome: cliente.Nome, idade: cliente.Idade);
+Cliente.ExibirInfo(email: cliente.Email, nome: cliente.Nome);
+
+Console.ReadKey();
+
+public struct Cliente
+{
+    public string Nome;
+    public string Email;
+
+    private int idade;
+    public int Idade
+    {
+        get { return idade; }
+        set
+        {
+            if (value < 18)
+            {
+                idade = 18;
+            }
+            else
+            {
+                idade = value;
+            }
+        }
+    }
+
+    public Cliente(string? nome, string? email, int idade)
+    {
+        Nome = nome;
+        Email = email;
+        Idade = idade;
+    }
+
+    public static void ExibirInfo(string nome, string email, int idade = 18)
+    {
+        Console.WriteLine($"{nome} {email} {idade}");
+    }
+}
