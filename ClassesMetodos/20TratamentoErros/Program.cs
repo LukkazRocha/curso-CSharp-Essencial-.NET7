2d8ff94fb853c4db9fbb4b0bfe3b706bc6dcd0cb
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-18 16:36:47 -0300

    finaliza seção sobre Classes e Métodos

---
+++ ClassesMetodos/20TratamentoErros/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("## Divisão de números inteiros ##\n");
+
+Console.WriteLine("\nInforme o valor de x ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nInforme o valor de y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+try
+{
+    int z = x / y;
+
+    Console.WriteLine($"\n{x} / {y} = {z}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine("\nNão existe divisão por zero, tente outro número...");
+    Console.WriteLine($"\nErro: <<< {ex.Message} >>>");
+    Console.WriteLine($"\nDetalhes {ex.StackTrace}");
+}
+finally
+{
+    Console.WriteLine("\nProcessamento concluído...");
+}
+
+Console.ReadKey();
---
+++ ClassesMetodos/21TiposAnonimos/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("## Tipos Anônimos ##\n");
+
+//var aluno = new
+//{
+//    Nome = "Maria",
+//    Idade = 43
+//};
+
+//Console.WriteLine(aluno.GetType().ToString());
+//Console.WriteLine(aluno.Nome + " " + aluno.Idade);
+
+////////////////////////////////////////////
+
+//var aluno = new
+//{
+//    Id = 1,
+//    Nome = "Jairo",
+//    Email = "jairo@email.com",
+//    Endereco = new { Id = 1, Cidade = "Santos", Pais = "Brasil" }
+//};
+
+//Console.WriteLine(aluno.Nome);
+//Console.WriteLine(aluno.Endereco.Cidade);
+
+////////////////////////////////////////////
+
+var alunos = new[]
+{
+    new { Id = 1, Nome = "Maria", Email = "maria@emmail.com" },
+    new { Id = 2, Nome = "Sonia", Email = "sonia@emmail.com" },
+    new { Id = 3, Nome = "Bruno", Email = "bruno@emmail.com" }
+};
+
+Console.WriteLine(alunos[0].Nome);
+Console.WriteLine(alunos[0].Email);
+
+Console.ReadKey();
