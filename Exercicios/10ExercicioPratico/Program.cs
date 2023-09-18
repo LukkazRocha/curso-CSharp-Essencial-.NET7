0ff57a2c9e3a2a534470948eef765741e63492f9
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-18 14:34:27 -0300

    realizando exercício 10

---
+++ Exercicios/10ExercicioPratico/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("## Exercício Prático 10 ##\n");
+
+Carro chevrolet = new Carro();
+chevrolet.Modelo = "Sedan";
+chevrolet.Montadora = "Chevrolet";
+chevrolet.Marca = "Onix";
+chevrolet.Ano = 2025;
+chevrolet.Potencia = 110;
+
+Console.WriteLine(Carro.ValorIpva);
+
+public class Carro
+{
+    public string? Modelo;
+    public string? Montadora;
+    public string? Marca;
+    public int Potencia;
+    public static double ValorIpva;
+
+    private int ano;
+    public int Ano
+    {
+        get { return ano; }
+        set
+        {
+            if (value < 2000)
+            {
+                ano = 2000;
+            }
+            else if (value > 2022)
+            {
+                ano = 2022;
+            }
+            else
+            {
+                ano = value;
+            }
+        }
+    }    
+
+    static Carro()
+    {
+        ValorIpva = 4;
+    }
+}
