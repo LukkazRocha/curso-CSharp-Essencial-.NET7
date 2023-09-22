0128fe6a0981c85cef918af7f1c93148b6e86d1f
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-22 15:27:17 -0300

    métodos básicos do List<T>

--- Colecoes/09List/Program.cs
+++ Colecoes/09List/Program.cs
@@ -34,14 +34,93 @@
 //////////////////////////////////////
 
 
-var lista = new List<string>() { "Maria", "Pedro", "Ana" };
+//var lista = new List<string>() { "Maria", "Pedro", "Ana" };
 
-string[] array1 = new string[] { "Diná", "Carlos", "Pedro" };
+//string[] array1 = new string[] { "Diná", "Carlos", "Pedro" };
 
-lista.AddRange(array1);
+//lista.AddRange(array1);
 
-lista.InsertRange(1, array1);
+//lista.InsertRange(1, array1);
 
-Console.WriteLine(lista.Count + " itens ");
+//Console.WriteLine(lista.Count + " itens ");
+
+
+//////////////////////////////////////
+
+
+//var lista = new List<int>() { 9, 5, 4, 1, 7, 8, 3, 6, 9, 0 };
+
+//lista.Remove(7);
+
+//lista.RemoveAt(4);
+
+//lista.RemoveRange(0, 2);
+
+
+//////////////////////////////////////
+
+
+var lista = new List<string>() { "Maria", "Ana", "Pedro", "Carlos", "Zé" };
+
+//var primeiro = lista[0];
+//var quarto = lista[3];
+
+//Console.WriteLine(primeiro);
+//Console.WriteLine(quarto);
+
+//lista[0] = "Zé";
+//lista[3] = "Silvia";
+
+//Console.WriteLine(primeiro);
+//Console.WriteLine(quarto);
+
+
+//////////////////////////////////////
+
+
+//for (int i = 0; i < lista.Count; i++)
+//{
+//    Console.WriteLine($"{lista[i]}");
+//}
+
+//foreach (var elemento in lista)
+//{
+//    Console.Write($"{elemento} ");
+//}
+
+
+//////////////////////////////////////
+
+
+//var res1 = lista.Contains("Ana");
+//var res2 = lista.Contains("Zé");
+
+//var res3 = lista.Contains("Ze");
+//var res4 = lista.Contains("Mari");
+
+//Console.WriteLine(lista.Contains("Pedro"));
+
+
+//////////////////////////////////////
+
+
+ExibirLista(lista);
+
+lista.Sort();
+
+ExibirLista(lista);
+
+lista.Clear();
+
+ExibirLista(lista);
+
+Console.WriteLine("Fim");
 
 Console.ReadKey();
+
+static void ExibirLista(List<string> lista)
+{
+    Console.WriteLine();
+    foreach (var item in lista)
+        Console.WriteLine(item);
+}
