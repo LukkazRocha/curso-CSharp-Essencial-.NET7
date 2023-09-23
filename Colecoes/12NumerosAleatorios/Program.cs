307a1bf886f5043b92b222220a7fa0b18bb13fd7
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-23 17:48:09 -0300

    finaliza aulas sobre coleções

---
+++ Colecoes/12NumerosAleatorios/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("## Números aleatórios ##\n");
+
+//byte[] randBytes = new byte[10];
+//Random random = new Random(2023);
+//random.NextBytes(randBytes);
+
+////Console.WriteLine(random.Next());
+////Console.WriteLine(random.Next(20)); //valor máximo
+////Console.WriteLine(random.Next(10, 20));//mínimo e máximo (pode incluir o mínimo mas não o máximo)
+////Console.WriteLine(random.NextDouble());
+
+//for (int i = 0; i < 6; i++)
+//{
+//    Console.WriteLine(randBytes[i]);
+//}
+
+
+////////////////////////////////////////////
+
+
+Console.WriteLine("Sorteio da MegaSena\n");
+
+Random random = new Random();
+int[] numerosSorteados = new int[6];
+
+for (int i = 0; i < 6; i++)
+{
+    int numeroAleatorio;
+    do
+    {
+        numeroAleatorio = random.Next(1, 61);
+    }
+    while (numerosSorteados.Contains(numeroAleatorio));
+
+    numerosSorteados[i] = numeroAleatorio;
+}
+
+Array.Sort(numerosSorteados);
+
+Console.WriteLine("Números Sorteados\n");
+
+Console.WriteLine(string.Join(" ", numerosSorteados));
+
+Console.ReadKey();
