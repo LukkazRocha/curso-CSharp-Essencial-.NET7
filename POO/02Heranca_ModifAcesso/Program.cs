1c4ff981ad814ed91fa449642d26103cf9f8b6db
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-09-25 20:11:38 -0300

    inicia estudos sobre herança

---
+++ POO/02Heranca_ModifAcesso/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("## Herança ##\n");
+
+ClasseDerivada cd = new();
+cd.VerificaAcesso();
+
+Console.ReadKey();
+
+public class ClasseDerivada : ClasseBase
+{
+    public void VerificaAcesso()
+    {
+        Public_Membro();
+        Protected_Membro();
+        Internal_Membro();
+        Protected_Internal_Membro();
+        //Private_Membro();
+
+        Console.WriteLine("\nAcesso ao campo private : private_var usando uma propriedade GET");
+        int soma = public_var + protected_var + internal_var + Private_var;
+        Console.WriteLine("\nTotal soma : " + soma);
+    }
+}
+
+public class ClasseBase
+{
+    //campos
+    public int public_var = 1;
+    protected int protected_var = 2;
+    internal int internal_var = 3;
+    private int private_var = 4;
+
+    //propriedades
+    public int Private_var
+    {
+        get { return private_var; }
+        set { private_var = value; }
+    }
+
+    public void Public_Membro()
+    {
+        Console.WriteLine("Classe Base - Método Public");
+    }
+    protected void Protected_Membro()
+    {
+        Console.WriteLine("Classe Base - Método Protected");
+    }
+    internal void Internal_Membro()
+    {
+        Console.WriteLine("Classe Base - Método Internal");
+    }
+    protected internal void Protected_Internal_Membro()
+    {
+        Console.WriteLine("Classe Base - Método Protected Internal");
+    }
+    private void Private_Membro()
+    {
+        Console.WriteLine("Classe Base - Método Private");
+    }
+}
---
+++ POO/03Heranca_Construtores/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("## Herança ##\n");
+
+Aluno aluno1 = new();
+Aluno aluno = new("José");
+
+Console.ReadKey();
+
+public class Pessoa
+{
+    public Pessoa()
+    {
+        Console.WriteLine("construtor da classe Pessoa");
+    }
+    public Pessoa(string nome)
+    {
+        Console.WriteLine("construtor da classe Pessoa com parâmetro");
+    }
+}
+
+public class Aluno : Pessoa
+{
+    public Aluno() : base()
+    {
+        Console.WriteLine("construtor da classe Aluno");
+    }
+    public Aluno(string nome) : base(nome)
+    {
+        Console.WriteLine("construtor da classe Aluno com parâmetro");
+    }
+}
