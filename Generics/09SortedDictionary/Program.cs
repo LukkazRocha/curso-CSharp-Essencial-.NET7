42018659947286297808a3a6e9f2dbad6aedddcc
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-10-14 17:14:34 -0300

    utilizando os principais métodos da coleção HashSet para consolidar conhecimentos

---
+++ Generics/09SortedDictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("## Criando o SortedDictionary ##\n");
+
+var brics = new SortedDictionary<int, string>()
+{
+    { 20, "Brasil" },
+    { 10, "China" },
+    { 40, "India" },
+    { 30, "URSS" },
+};
+
+Console.WriteLine("\nIncluindo o elemento de chave=70 e valor =África do Sul");
+if(brics.ContainsKey(70))
+{
+    Console.WriteLine("Chave já existe");
+}
+else
+{
+    brics.Add(70, "África do Sul");
+}
+
+Console.WriteLine($"Número de pares de chaves(elemento) no dicionário : {brics.Count}");
+
+Console.WriteLine("\n-Acessando um elemento pela chave -> brics[20]");
+Console.WriteLine($"Chave 20 -> {brics[20]}");
+
+Console.WriteLine("\n-Alterando o valor da chave 20 para Argentina -> brics[20] = Argentina");
+brics[20] = "Argentina";
+Console.WriteLine($"Chave 20 -> {brics[20]}");
+
+Console.WriteLine("\n-Removendo a chave 10 : brics.Remove[10] -> China");
+brics.Remove(10);
+
+Console.WriteLine("\n-Verificando se uma chave existe com ContainsKey");
+Console.WriteLine($"Existe a chave : 30 ? -> {brics.ContainsKey(30)}");
+
+Console.WriteLine("\nVerificando se um valor existe com ContainsValue");
+Console.WriteLine($"Exite o valor : 'URSS' -> {brics.ContainsValue("URSS")}");
+Console.WriteLine($"Exite o valor : 'Chile' -> {brics.ContainsValue("Chile")}");
+
+Console.WriteLine("\n-Obtendo o valor da chave 30 usando o TryGetValue");
+
+if (brics.TryGetValue(30, out string? valor))
+{
+    Console.WriteLine($"Valor da chave 30 : {valor}");
+}
+else
+{
+    Console.WriteLine("A chave não existe.");
+}
+
+Console.WriteLine("\nPercorrendo um SortedDictionary com foreach\n");
+foreach (var pais in brics)
+{
+    Console.WriteLine($"Chave/Valor = {pais.Key} {pais.Value}");
+}
+
+Console.ReadKey();
---
+++ Generics/10HashSet/Program.cs
@@ -0,0 +1,103 @@
+﻿
+var timesSP = new HashSet<string> { "Santos", "Palmeiras", "São Paulo" };
+var timesRJ = new HashSet<string> { "Vasco", "Flamengo", "Fluminense" };
+var timesBA = new HashSet<string> { "Bahia", "Vitória", "Juazeiro" };
+var timesMundiais = new HashSet<string> { "Santos", "Palmeiras", "São Paulo", "Flamengo" };
+
+if (!timesSP.Contains("Corinthians"))
+{
+    timesSP.Add("Corinthians");
+}
+//var resultado = timesBA.Remove("Juazeiro");
+
+//Verifica se a coleção de timesSP esta contida em timesMundiais
+
+
+////////////////////////////////////////////////
+
+
+
+//if (timesSP.IsSubsetOf(timesMundiais))
+//{
+//    Console.WriteLine("timesSP é um subconjunto de timesMundiais\n");
+//}
+
+////Verifica se timesMundiais contem a coleção timesSP
+//if (timesMundiais.IsSupersetOf(timesSP))
+//{
+//    Console.WriteLine("timesMundiais é um superconjunto de timesSP\n");
+//}
+
+////Verifica se existem elementos em comum entre timesRJ e timesMundiais
+//if (timesRJ.Overlaps(timesMundiais))
+//{
+//    Console.WriteLine("Pelo menos um time do RJ tem título mundial");
+//}
+
+////Verifica se duas coleções não são iguais
+//if (!timesSP.SetEquals(timesRJ))
+//{
+//    Console.WriteLine("\ntimesSP e timesRJ não contém os mesmos elementos");
+//}
+
+
+////////////////////////////////////////////////
+
+
+//Console.WriteLine("\nJuntando os HashSet<T> : SP, RJ e BA");
+//timesSP.UnionWith(timesRJ);
+//timesSP.UnionWith(timesBA);
+
+//ExibirColecao(timesSP);
+
+//Console.WriteLine("\nJuntando os times de SP, RJ e BA ordenados");
+//var todosTimes = new SortedSet<string>(timesSP);
+
+//ExibirColecao(todosTimes);
+
+
+////////////////////////////////////////////////
+
+
+//junta os times em comum entre timesSP e timesMundiais
+//Console.WriteLine("\nInterseção de dois HashSet<T> : timesAP e timesMundiais");
+//timesSP.IntersectWith(timesMundiais);
+//ExibirColecao(timesSP);
+
+
+////////////////////////////////////////////////
+
+
+//remove os times em comum entre timesSP e timesMundiais, mantem somente a diferença que existe no segundo (timesSP)
+//Console.WriteLine("\nDiferença de dois HashSet<T> : timesMundiais e timesSP");
+//timesMundiais.ExceptWith(timesSP);
+//ExibirColecao(timesMundiais);
+
+
+////////////////////////////////////////////////
+
+
+//remove os times em comum entre timesSP e timesMundiais, mantem somente a diferença que existe nos dois
+//Console.WriteLine("\nDiferença de dois HashSet<T> : timesMundiais e timesSP com SymmetricExceptWith");
+//timesMundiais.SymmetricExceptWith(timesSP);
+//ExibirColecao(timesMundiais);
+
+
+////////////////////////////////////////////////
+
+
+Console.WriteLine("\nRemove todos os elementos de timesMundiais");
+timesMundiais.Clear();
+ExibirColecao(timesMundiais);
+Console.WriteLine("Fim do processamento...");
+
+Console.ReadKey();
+
+static void ExibirColecao<T>(IEnumerable<T> colecao)
+{
+    Console.WriteLine();
+    foreach (var item in colecao)
+    {
+        Console.WriteLine($"{item}");
+    }
+}
