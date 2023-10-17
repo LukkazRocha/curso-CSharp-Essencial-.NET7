c126544dd404f3dafb6a8e082a5cfb0d72f469c5
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2023-10-17 14:02:13 -0300

    exemplos da coleções stack e queue e inicia exercicio para consolidar conhecimentos sobre ambos

---
+++ Generics/13StackUndo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using _13StackUndo;
+
+Console.WriteLine("\n### Stack - Undo ###");
+
+EditorTexto editor = new EditorTexto();
+Console.WriteLine("\nDigitando...");
+editor.DigitarChar('S');
+editor.DigitarChar('t');
+editor.DigitarChar('a');
+editor.DigitarChar('c');
+editor.DigitarChar('q');
+
+Console.WriteLine("\nFazendo o Undo...\n");
+editor.Undo();
+editor.Undo();
+
+Console.WriteLine("\nRedigitando...\n");
+editor.DigitarChar('c');
+editor.DigitarChar('k');
+editor.DigitarChar('!');
+
+Console.ReadKey();
