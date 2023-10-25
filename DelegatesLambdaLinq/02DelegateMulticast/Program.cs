42a5fa85898084f6ce5ac6fb69292b2db717abd8
LukkazRocha <108117674+LukkazRocha@users.noreply.github.com>
2023-10-25 13:57:27 -0300

    exemplos de delegates e multicast delegate

---
+++ DelegatesLambdaLinq/02DelegateMulticast/Program.cs
@@ -0,0 +1,28 @@
+﻿
+MeuDelegate delegateMulticast = new MeuDelegate(Metodo1);
+delegateMulticast += Metodo2;
+delegateMulticast += Metodo3;
+
+delegateMulticast("Olá, multicast delegate!");
+
+delegateMulticast -= Metodo3;
+delegateMulticast("O Método3 foi removido...");
+
+Console.ReadKey();
+
+static void Metodo1(string mensagem)
+{
+    Console.WriteLine("Método 1: " + mensagem);
+}
+
+static void Metodo2(string mensagem)
+{
+    Console.WriteLine("Método 2: " + mensagem);
+}
+
+static void Metodo3(string mensagem)
+{
+    Console.WriteLine("Método 3: " + mensagem);
+}
+
+public delegate void MeuDelegate(string mensagem);
---
+++ DelegatesLambdaLinq/03MetodosAnonimos/Program.cs
@@ -0,0 +1,26 @@
+﻿//Imprimir imprimir = delegate (int valor)
+//{
+//    Console.WriteLine($"Método Anônimo: {valor}");
+//};
+
+//imprimir(100);
+//imprimir(200);
+
+List<string> nomes = new();
+nomes.Add("Maria");
+nomes.Add("Miriam");
+nomes.Add("Paulo");
+nomes.Add("Carlos");
+
+//string resultado = nomes.Find(VerificaNomeNaLista);
+
+string? resultado = nomes.Find(delegate (string nome)
+{
+    return nome.Equals("Paulo");
+});
+
+Console.WriteLine(resultado);
+
+Console.ReadKey();
+
+//public delegate void Imprimir(int valor);
