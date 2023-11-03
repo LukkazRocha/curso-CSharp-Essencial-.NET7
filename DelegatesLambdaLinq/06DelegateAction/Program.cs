2846fba9e5a1faaa3e2047d4e79e002882ff43bc
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-11-03 13:37:39 -0300

    estudos com exemplos de Delegate Action e Func

---
+++ DelegatesLambdaLinq/06DelegateAction/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Informe um número inteiro:");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+//Action<int> dobra = Duplicar;
+//Action<int> dobra = delegate (int x)
+//{
+//    Console.WriteLine(x * 2);
+//};
+Action<int> dobra = x => Console.WriteLine(x * 2);
+
+dobra(numero);
+
+Console.ReadKey();
+
+//static void Duplicar(int num)
+//{
+//    int resultado = num * 2;
+//    Console.WriteLine(resultado);
+//}
---
+++ DelegatesLambdaLinq/07DelegateFunc/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Digite um número:");
+double numero = Convert.ToDouble(Console.ReadLine());
+
+//Func<double, double> raizQuadrada = CalculaRaiz;
+//Func<double, double> raizQuadrada = delegate (double x)
+//{
+//    return Math.Sqrt(x);
+//};
+
+Func<double, double> raizQuadrada = Math.Sqrt;
+
+double resultado = raizQuadrada(numero);
+
+Console.WriteLine($"\nA raiz quadrada de {numero} é {resultado}");
+
+Console.ReadKey();
+
+//static double CalculaRaiz(double numero)
+//{
+//    return Math.Sqrt(numero);
+//}
