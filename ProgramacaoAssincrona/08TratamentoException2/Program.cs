7c7666063c56e35145c3042de452fa099e89e262
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-11-26 17:13:54 -0300

    múltiplas exceções

---
+++ ProgramacaoAssincrona/08TratamentoException2/Program.cs
@@ -0,0 +1,32 @@
+﻿await LancaMultiplasExcecoesAsync();
+Console.ReadKey();
+
+static async Task LancaMultiplasExcecoesAsync()
+{
+    Task? tarefas = null;
+    try
+    {
+        var primeiraTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new IndexOutOfRangeException("IndexOutOfRangeException lançada explicitamente");
+        });
+        var segundaTask = Task.Run(() =>
+        {
+            Task.Delay(1000);
+            throw new InvalidOperationException("InvalidOperationException lançada explicitamente");
+        });
+        tarefas = Task.WhenAll(primeiraTask, segundaTask);
+        await tarefas;
+    }
+    catch
+    {
+        Console.WriteLine("Ocorreram as seguintes exceções :-\n");
+        AggregateException TodasExceptions = tarefas.Exception;
+
+        foreach (var ex in TodasExceptions.InnerExceptions)
+        {
+            Console.WriteLine(ex.GetType().ToString() + "\t\t" + ex.Message);
+        }
+    }
+}
