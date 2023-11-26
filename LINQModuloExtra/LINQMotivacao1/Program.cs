dcc07a5d35b69d136ccbc9b242b3f45435e101ca
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-11-26 18:12:23 -0300

    iniciando módulo extra sobre LINQ

---
+++ LINQModuloExtra/LINQMotivacao1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("## LINQ ##\n");
+
+List<string> frutas = new() { "Banana", "Maçã", "Pera", "Laranja", "Uva" };
+
+//query syntax
+
+var resultado = from f in frutas
+                where f.Contains('r')
+                select f;
+
+Console.WriteLine(String.Join(" ", resultado));
+
+Console.ReadKey();
