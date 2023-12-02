d02e9f25c50af0a086c9d51674c7462a4b71b6cc
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-12-02 19:52:48 -0300

    iniciando estudos extras sobre LINQ
    primeiro exemplo de consultas

---
+++ LINQModuloExtra/LINQMotivacao1/FonteDados.cs
@@ -0,0 +1,33 @@
+﻿namespace LINQMotivacao1
+{
+    public class FonteDados
+    {
+        public static List<int> GetNumeros()
+        {
+            return new List<int>
+            {
+                1, 2, 4, 8, 16, 32, 64, 128, 256, 512
+            };
+        }
+
+        public static List<int> GetListaNegra()
+        {
+            return new List<int>
+            {
+                16, 128, 512
+            };
+        }
+
+        public static List<Aluno> GetAlunos()
+        {
+            return new List<Aluno>
+            {
+                new() { Nome = "Maria", Idade = 42 },
+                new() { Nome = "Manoel", Idade = 34 },
+                new() { Nome = "Amanda", Idade = 21 },
+                new() { Nome = "Carlos", Idade = 18 },
+                new() { Nome = "Alicia", Idade = 15 },
+            };
+        }
+    }
+}
--- LINQModuloExtra/LINQMotivacao1/Program.cs
+++ LINQModuloExtra/LINQMotivacao1/Program.cs
@@ -8,6 +8,10 @@ var resultado = from f in frutas
                 where f.Contains('r')
                 select f;
 
-Console.WriteLine(String.Join(" ", resultado));
+Console.WriteLine(String.Join(" - ", resultado));
+
+//method syntax
+var resultado2 = frutas.Where(f => f.Contains('r'));
+Console.WriteLine(String.Join(" - ", resultado2));
 
 Console.ReadKey();
