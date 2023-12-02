3cfdc4eedc2fed9409a71eb4cdfe2f4bda91849a
Lucas Rocha <108117674+LukkazRocha@users.noreply.github.com>
2023-12-02 16:07:50 -0300

    exercicio sobre operações assíncronas

--- LINQModuloExtra/LINQMotivacao1/obj/Debug/net7.0/LINQMotivacao1.AssemblyInfo.cs
+++ LINQModuloExtra/LINQMotivacao1/obj/Debug/net7.0/LINQMotivacao1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LINQMotivacao1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7666063c56e35145c3042de452fa099e89e262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LINQMotivacao1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LINQMotivacao1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
